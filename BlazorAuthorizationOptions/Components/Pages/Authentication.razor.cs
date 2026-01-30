ed9752b8f5ef6946310ee983b07134f08e126705
Lasse Herget <Lasse.Herget@swisssalary.ch>
2026-01-30 11:22:55 +0100

    Clean up usings in Authentication.razor.cs
    Removed unused using directives and added Microsoft.AspNetCore.Components to streamline dependencies and support Blazor component features.

--- BlazorAuthorizationOptions/Components/Pages/Authentication.razor.cs
+++ BlazorAuthorizationOptions/Components/Pages/Authentication.razor.cs
@@ -1,9 +1,5 @@
-﻿using Microsoft.AspNetCore.Authentication;
-using Microsoft.AspNetCore.Authentication.Cookies;
-using Microsoft.AspNetCore.Authorization;
+﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Components;
-using System.Data;
-using System.Security.Claims;
 
 namespace BlazorAuthorizationOptions.Components.Pages;
 
